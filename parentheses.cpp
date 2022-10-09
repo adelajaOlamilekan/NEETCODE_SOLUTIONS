caf332d9f94033a32590e9ba349553fe89cef5e1
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2022-10-09 17:24:49 +0100

    Parentheses parser solution code
    Problem link: https://leetcode.com/problems/valid-parentheses/description/

---
+++ parentheses.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+#include<map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        map<char, char>bracket_check;
+        bracket_check[')'] = '(';
+        bracket_check[']'] = '[';
+        bracket_check['}'] = '{';
+
+        stack<char> sign;
+
+        for(char c : s){
+            if (c == '(' || c == '[' || c == '{'){
+                sign.push(c);
+            }
+            else if ( (c == ')' || c == ']' || c == '}') && sign.empty()){
+                sign.push(c);
+            }
+            else if ( bracket_check[c] == sign.top() ){
+                sign.pop();
+            }
+            else{
+                break;
+            }
+        }
+
+        return sign.empty(); 
+        
+    }
+};
